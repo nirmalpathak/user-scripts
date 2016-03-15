504f8e559ddd7239a6544360417440d376fbc432
Nirmal Pathak <ndpathak@yahoo.com>
2016-03-14 17:05:02 -0700

    Adding file close method.

--- adduser.py
+++ adduser.py
@@ -11,5 +11,6 @@ def user_mgmt(usernames,sgroup):
 		key = open('/home/%s/.ssh/id_rsa.pub' %user, 'r').read().replace('\n', '') #Get SSH Keys
 		os.system("usermod -p '%s' %s" %(key,user)) #Replace password with SSH public keys.
 		os.system("passwd -l %s" %user) #Lock user password.
+		key.close()
 
 user_mgmt(["user1","user2"], 'users')
